7ea58a9c03621e61032f05ea03c8af72a969bbd8
12753080 <sos681201@gmail.com>
2024-09-26 16:21:20 +0800

    Add files via upload

---
+++ week03/week03-1.cpp
@@ -0,0 +1,17 @@
+///week03-1.cpp了解"文法糖"range-base for迴圈在2011年C++才能用
+///warning: range-based 'for' loops only available with - std=c++11 or - std=
+///CodeBlocks-Settings-Compler 把 - std=c++11的2011年的C++開起來
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+    for(char c : s){
+        cout << c << "\n";
+    }
+}
+
+
